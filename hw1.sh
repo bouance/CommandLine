9c63cb9b898b26ce0aa9dc0ddfe0a1d030d2b473
bouance <bouancedan@gmail.com>
2018-10-15 20:12:38 +0300

    i add my homework

---
+++ hw1.sh
@@ -0,0 +1,23 @@
+mkdir directory
+cd dirctory
+touch blank.txt
+touch greetings.txt
+echo "hello"> greetings .txt
+echo "hello">>greetings.txt
+echo "hello">>greetings.txt
+echo "hello">>greetings.txt
+echo "hello">>greetings.txt
+cpgreetings.txt1.txt
+cpgreetings.txt2.txt
+cpgreetings.txt3.txt
+cpgreetings.txt4.txt
+cpgreetings.txt5.txt
+echo"cat>pets.txt
+echo "dog">>pets.txt
+echo :hamster">>pets.txt
+
+echo "cat"> commads.txt
+echo "ls">>commad.txt
+echo"pwd" >>command.txt
+#cat petsss.txt comammads.txt /sort/ uniq lovelycommands.txt
+diff pets.txt commands.txt>>lovelycommands.txt
